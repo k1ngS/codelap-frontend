fd57d1e9a6f182d9331d8ca21d87e69b01e87434
k1ngS <marcos.beltrao@outlook.pt>
2025-07-25 18:34:32 -0300

    feat: implement Header component with styled layout and logout functionality

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,12 @@
+import { useUser } from '../../context/UserContext'
 import * as S from './styles'
 
 const Header = () => {
+  const { logout } = useUser();
   return (
     <S.Container>
       <S.Title>CodeLeap Network</S.Title>
+      <S.Button type='button' onClick={() => logout()}>Logout</S.Button>
     </S.Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,12 +3,38 @@ import styled from 'styled-components'
 export const Container = styled.header`
   background-color: #7695EC;
   padding: 27px 37px;
-  text-align: center;
+  margin-bottom: 24px;
+
+  display: flex;
+  justify-content: space-between;
+  width: 800px;
+  margin: 0 auto;
 `
 
 export const Title = styled.h1`
+  grid-column: 2;
   color: white;
   font-size: 22px;
   font-weight: 700;
   margin: 0;
+  justify-self: center;
+`
+
+export const Button = styled.button`
+  grid-column: 3;
+  justify-self: end;
+
+  background-color: #FFFFFF;
+  border: none;
+  border-radius: 8px;
+  color: #000;
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: 700;
+  padding: 8px 20px;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.8;
+  }
 `
