d22afaa95487c8c5f14d5aa93ea800753f7dee5a
k1ngS <marcos.beltrao@outlook.pt>
2025-07-25 16:21:39 -0300

    feat: add UserContext for managing user authentication state

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,4 +1,6 @@
-import { useContext, useState } from 'react'
+import { createContext, useContext, useState } from 'react'
+
+const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [username, setUsername] = useState('');
